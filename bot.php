445082be64ca3ffb343f2d20e13589567303156d
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2024-02-02 22:44:40 +0330

    V0.2 🚀

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ function fetchContent($url) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Accept-Language: en-US,en;q=0.9',
+        'Accept-Language: en-US,fa;q=0.9',
         'Cookie: messagesDesktopMode=0;'
     ]);
     $response = curl_exec($ch);
@@ -26,20 +26,13 @@ function extractConfigurations($content) {
     $H2Pattern = '/hy2:\/\/[^<>\'"]+/';
     $tuicPattern = '/tuic:\/\/[^<>\'"]+/';
 
-    preg_match_all($vlessPattern, $content, $vlessMatches);
-    preg_match_all($vmessPattern, $content, $vmessMatches);
-    preg_match_all($ssPattern, $content, $ssMatches);
-    preg_match_all($trojanPattern, $content, $trojanMatches);
-    preg_match_all($H2Pattern, $content, $H2Matches);
-    preg_match_all($tuicPattern, $content, $tuicMatches);
-
     return [
-        implode(PHP_EOL, !empty($vlessMatches[0]) ? $vlessMatches[0] : []),
-        implode(PHP_EOL, !empty($vmessMatches[0]) ? $vmessMatches[0] : []),
-        implode(PHP_EOL, !empty($ssMatches[0]) ? $ssMatches[0] : []),
-        implode(PHP_EOL, !empty($trojanMatches[0]) ? $trojanMatches[0] : []),
-        implode(PHP_EOL, !empty($H2Matches[0]) ? $H2Matches[0] : []),
-        implode(PHP_EOL, !empty($tuicMatches[0]) ? $tuicMatches[0] : []),
+        implode(PHP_EOL, preg_match_all($vlessPattern, $content, $vlessMatches) ? $vlessMatches[0] : []),
+        implode(PHP_EOL, preg_match_all($vmessPattern, $content, $vmessMatches) ? $vmessMatches[0] : []),
+        implode(PHP_EOL, preg_match_all($ssPattern, $content, $ssMatches) ? $ssMatches[0] : []),
+        implode(PHP_EOL, preg_match_all($trojanPattern, $content, $trojanMatches) ? $trojanMatches[0] : []),
+        implode(PHP_EOL, preg_match_all($H2Pattern, $content, $H2Matches) ? $H2Matches[0] : []),
+        implode(PHP_EOL, preg_match_all($tuicPattern, $content, $tuicMatches) ? $tuicMatches[0] : []),
     ];
 }
 
@@ -47,62 +40,50 @@ function generateTrojanConfig() {
     $currentDateTime = date('D-d-M-Y H:i');
     return "trojan://bcacaab-baca-baca-dbac-accaabbcbacb@127.0.0.1:1080?security=tls&type=tcp#%F0%9F%94%84%20LATEST-UPDATE%20%F0%9F%93%85%20{$currentDateTime}";
 }
+
 function Signature() {
     return "trojan://bcacaab-baca-baca-dbac-accaabbcbacb@127.0.0.1:8080?security=tls&type=tcp#%C2%A9Made%20by:%20github.com/MhdiTaheri%20%F0%9F%93%8C";
 }
 
-$allVlessConfigs = [];
-$allVMessConfigs = [];
-$allSSConfigs = [];
-$allTrojanConfigs = [];
-$allH2Configs = [];
-$alltuicConfigs = [];
+$allVlessConfigs = $allVMessConfigs = $allSSConfigs = $allTrojanConfigs = $allH2Configs = $alltuicConfigs = [];
 
 foreach ($telegramChannelURLs as $channelURL) {
     $channelContent = fetchContent($channelURL);
 
     if ($channelContent !== false) {
-        [$vless, $vmess, $ss, $trojan, $H2, $tuic] = extractConfigurations($channelContent);
-
-        if (!empty($vless)) {
-            $allVlessConfigs[] = $vless;
-        }
-
-        if (!empty($vmess)) {
-            $allVMessConfigs[] = $vmess;
-        }
-
-        if (!empty($ss)) {
-            $allSSConfigs[] = $ss;
-        }
-
-        if (!empty($trojan)) {
-            $allTrojanConfigs[] = $trojan;
-        }
-
-        if (!empty($H2)) {
-            $allH2Configs[] = $H2;
-        }
-
-        if (!empty($tuic)) {
-            $alltuicConfigs[] = $tuic;
-        }
+        [
+            $allVlessConfigs[],
+            $allVMessConfigs[],
+            $allSSConfigs[],
+            $allTrojanConfigs[],
+            $allH2Configs[],
+            $alltuicConfigs[],
+        ] = extractConfigurations($channelContent);
     }
 }
 
-file_put_contents('vless.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allVlessConfigs) . PHP_EOL . Signature());
-file_put_contents('vmess.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allVMessConfigs) . PHP_EOL . Signature());
-file_put_contents('ss.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allSSConfigs) . PHP_EOL . Signature());
-file_put_contents('trojan.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allTrojanConfigs) . PHP_EOL . Signature());
-file_put_contents('hysteria.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allH2Configs) . PHP_EOL . Signature());
-file_put_contents('tuic.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $alltuicConfigs) . PHP_EOL . Signature());
-file_put_contents('mix.txt', generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allVlessConfigs) . PHP_EOL . implode(PHP_EOL, $allVMessConfigs) . PHP_EOL . implode(PHP_EOL, $allSSConfigs) . PHP_EOL . implode(PHP_EOL, $allTrojanConfigs) . PHP_EOL . implode(PHP_EOL, $allH2Configs) . PHP_EOL . implode(PHP_EOL, $alltuicConfigs) . PHP_EOL . Signature());
-// base64
-file_put_contents('vlessbase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allVlessConfigs) . PHP_EOL . Signature()));
-file_put_contents('vmessbase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allVMessConfigs) . PHP_EOL . Signature()));
-file_put_contents('ssbase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allSSConfigs) . PHP_EOL . Signature()));
-file_put_contents('trojanbase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allTrojanConfigs) . PHP_EOL . Signature()));
-file_put_contents('hysteriabase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allH2Configs) . PHP_EOL . Signature()));
-file_put_contents('tuicbase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $alltuicConfigs) . PHP_EOL . Signature()));
-file_put_contents('mixbase64.txt', base64_encode(generateTrojanConfig() . PHP_EOL . implode(PHP_EOL, $allVlessConfigs) . PHP_EOL . implode(PHP_EOL, $allVMessConfigs) . PHP_EOL . implode(PHP_EOL, $allSSConfigs) . PHP_EOL . implode(PHP_EOL, $allTrojanConfigs) . PHP_EOL . implode(PHP_EOL, $allH2Configs) . PHP_EOL . implode(PHP_EOL, $alltuicConfigs) . PHP_EOL . Signature()));
+$trojanConfig = generateTrojanConfig();
+$signature = Signature();
+
+$fileContents = [
+    'vless' => $trojanConfig . PHP_EOL . implode(PHP_EOL, $allVlessConfigs) . PHP_EOL . $signature,
+    'vmess' => $trojanConfig . PHP_EOL . implode(PHP_EOL, $allVMessConfigs) . PHP_EOL . $signature,
+    'ss' => $trojanConfig . PHP_EOL . implode(PHP_EOL, $allSSConfigs) . PHP_EOL . $signature,
+    'trojan' => $trojanConfig . PHP_EOL . implode(PHP_EOL, $allTrojanConfigs) . PHP_EOL . $signature,
+    'hysteria' => $trojanConfig . PHP_EOL . implode(PHP_EOL, $allH2Configs) . PHP_EOL . $signature,
+    'tuic' => $trojanConfig . PHP_EOL . implode(PHP_EOL, $alltuicConfigs) . PHP_EOL . $signature,
+    'mix' => $trojanConfig . PHP_EOL .
+        implode(PHP_EOL, $allVlessConfigs) . PHP_EOL .
+        implode(PHP_EOL, $allVMessConfigs) . PHP_EOL .
+        implode(PHP_EOL, $allSSConfigs) . PHP_EOL .
+        implode(PHP_EOL, $allTrojanConfigs) . PHP_EOL .
+        implode(PHP_EOL, $allH2Configs) . PHP_EOL .
+        implode(PHP_EOL, $alltuicConfigs) . PHP_EOL .
+        $signature,
+];
+
+foreach ($fileContents as $key => $content) {
+    file_put_contents("sub/{$key}", $content);
+    file_put_contents("sub/{$key}base64", base64_encode($content));
+}
 ?>
